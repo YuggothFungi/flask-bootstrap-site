a75bf665f9e2d49d833ab128f1bdc915500a873f
Nikita Kharitonov <nkharitonov@gmail.com>
2024-04-16 22:07:28 +0300

    added flask sessions handling

--- app.py
+++ app.py
@@ -1,30 +1,37 @@
+import sys
 from db import getUserList
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 USERTYPES = ["Учащийся", "Учитель", "Администратор"]
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', name=session.get("name"))
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        if not request.form.get("loginName"):
+            return render_template('error.html', message="Вы не ввели логин")
+        session["name"] = request.form.get("loginName")
+        return redirect("/")
     return render_template('login.html')
 
-@app.route("/course", methods=["POST"])
+@app.route("/auth", methods=["POST"])
 def course():
-    if not request.form.get("loginName"):
-        return render_template('error.html', message="Вы не ввели логин")
-    else:
-        name = request.form.get("loginName")
-        return render_template('course.html', name=name)
+    name = session.get("name")
+    return render_template('course.html', name=name)
     
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        if request.form.get("usertype") not in USERTYPES:
+        if request.form.get("registerUsertype") not in USERTYPES:
             return render_template("error.html", message="Некорректная роль пользователя")
         return redirect("/users")
     return render_template("register.html", usertypes=USERTYPES)
