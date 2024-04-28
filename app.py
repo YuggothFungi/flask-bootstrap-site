5e9d63c41eb39dfcbbd1471e5d994f23d7f67e51
Nikita Kharitonov <nkharitonov@gmail.com>
2024-04-28 17:56:51 +0300

    added passw confirmation check, register nav link

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-USERTYPES = ["Учащийся", "Учитель", "Администратор"]
+USERTYPES = { "1": "Учащийся"
+            , "2" : "Учитель"
+            , "0" : "Администратор"
+            }
 
 @app.route("/")
 def index():
@@ -21,45 +24,41 @@ def login():
                 return render_template('error.html', message="Вы не ввели логин")
             session['logged_in']=True
             name = session['name'] = request.form.get("loginName")
-            print(name)
             if checkUserRole(name) == 0:
                 return redirect("/course1")
             else:
                 return redirect("/register")
         
-        session['logged_in'] = False
-        session['name'] = None
+        session.clear()
         return redirect("/")
     return render_template('login.html')
 
 @app.route("/course1")
 def course1():
-    name = session.get("name")
     return render_template('course1.html')
 
 @app.route("/course2")
 def course2():
-    name = session.get("name")
     return render_template('course2.html')
 
 @app.route("/course3")
 def course3():
-    name = session.get("name")
     return render_template('course3.html')
 
 @app.route("/course4")
 def course4():
-    name = session.get("name")
     return render_template('course4.html')
 
 @app.route("/course5")
 def course5():
-    name = session.get("name")
     return render_template('course5.html')
     
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        if request.form.get("registerPassword") != request.form.get("registerRepeatPassword"):
+            return render_template("error.html", message="Пароли не совпадают")
+
         if request.form.get("registerUsertype") not in USERTYPES:
             return render_template("error.html", message="Некорректная роль пользователя")
         return redirect("/users")
